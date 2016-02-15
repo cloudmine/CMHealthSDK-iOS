9bf891c10c1e7af0402a61dd644b3ab7f91eb911
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-02-15 12:45:39 -0500

    Implement a top level static method for setting up app credenitials, negating the last direct user dependency on the CloudMine SDK

--- Pod/Classes/CMHealth.h
+++ Pod/Classes/CMHealth.h
@@ -1,8 +1,13 @@
-#ifndef CMHealth_h
-#define CMHealth_h
+#import <Foundation/Foundation.h>
+#import <ResearchKit/ResearchKit.h>
+#import <CloudMine/CloudMine.h>
 
 #import "CMHUserData.h"
 #import "CMHUser.h"
 #import "ORKResult+CMHealth.h"
 
-#endif
+@interface CMHealth : NSObject
+
++ (void)setAppIdentifier:(NSString *_Nonnull)identifier appSecret:(NSString *_Nonnull)secret;
+
+@end
---
+++ Pod/Classes/CMHealth.m
@@ -0,0 +1,15 @@
+#import "CMHealth.h"
+
+@implementation CMHealth
+
++ (void)setAppIdentifier:(NSString *_Nonnull)identifier appSecret:(NSString *_Nonnull)secret
+{
+    NSAssert(nil != identifier, @"CMHealth App Identifier can not be nil");
+    NSAssert(nil != secret, @"CMHealth App Secret can not be nil");
+
+    CMAPICredentials *credentials = [CMAPICredentials sharedInstance];
+    credentials.appIdentifier = identifier;
+    credentials.appSecret = secret;
+}
+
+@end
