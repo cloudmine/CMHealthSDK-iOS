9d4afff04d96cc793aaddd386f8d138652ed9d6c
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-02-17 15:38:44 -0500

    Use Apple recommended format for error domain string

--- Pod/Classes/CMHErrors.h
+++ Pod/Classes/CMHErrors.h
@@ -3,7 +3,7 @@
 #ifndef CMHErrors_h
 #define CMHErrors_h
 
-static NSString *const CMHErrorDomain = @"CMHErrorDomain";
+static NSString *const CMHErrorDomain = @"me.cloudmine.CMHealth.ErrorDomain";
 
 typedef NS_ENUM(NSUInteger, CMHError) {
     CMHErrorUserMissingConsent      = 700,
