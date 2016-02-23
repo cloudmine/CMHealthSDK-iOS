573e97ed3733b13514dfa3295cfbe1b27b50d882
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-02-23 14:54:16 -0500

    Import layout for styled Signup screen

--- Pod/Classes/CMHSignupViewController.m
+++ Pod/Classes/CMHSignupViewController.m
@@ -14,9 +14,9 @@
 
 + (_Nonnull instancetype)signupViewController
 {
-    UIViewController *vc = [UIStoryboard storyboardWithName:@"CMHSignup" bundle:[CMHBundler instance].bundle].instantiateInitialViewController;
-    NSAssert(nil != vc, @"");
-    NSAssert([vc isKindOfClass:[self class]], @"");
+    UIViewController *vc = [[UIStoryboard storyboardWithName:@"CMHSignup" bundle:[CMHBundler instance].bundle] instantiateInitialViewController];
+    NSAssert(nil != vc, @"Failed to load CMHSignupViewController from Storyboard");
+    NSAssert([vc isKindOfClass:[self class]], @"Expected to load %@ but got %@", [self class], [vc class]);
     return (CMHSignupViewController *)vc;
 }
 
