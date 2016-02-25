9c36fe030abf9c7153066219c4151c39745e06b6
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-02-24 22:56:57 -0500

    Display an alert view when the user chooses reset password; collect and validate the account email adress

--- Pod/Classes/CMHAuthViewController.m
+++ Pod/Classes/CMHAuthViewController.m
@@ -86,9 +86,29 @@ static NSString *const CMHAuthStoryboardName = @"CMHAuth";
 {
     [self.delegate authViewCancelledType:self.authType];
 }
+
 - (IBAction)forgotPasswordButtonDidPress:(UIButton *)sender
 {
+    UIAlertController *resetAlert = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Reset Password", nil)
+                                                                        message:NSLocalizedString(@"Please enter the email address you used to create your account", nil)
+                                                                 preferredStyle:UIAlertControllerStyleAlert];
 
+    [resetAlert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = NSLocalizedString(@"Email", nil);
+        textField.keyboardType = UIKeyboardTypeEmailAddress;
+    }];
+
+    [resetAlert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", nil)
+                                                   style:UIAlertActionStyleCancel
+                                                 handler:^(UIAlertAction * _Nonnull action) { }]];
+
+    UIAlertAction *resetAction = [UIAlertAction actionWithTitle:NSLocalizedString(@"Reset", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSString *email = resetAlert.textFields[0].text;
+        [self resetPasswordForAccountWithEmail:email];
+    }];
+    [resetAlert addAction:resetAction];
+
+    [self presentViewController:resetAlert animated:YES completion:nil];
 }
 
 #pragma mark Notifications
@@ -113,6 +133,15 @@ static NSString *const CMHAuthStoryboardName = @"CMHAuth";
 - (BOOL)hasEnteredPasswordText
 {
     return nil != self.passwordTextField.text && self.passwordTextField.text.length > 5;
+}
+
+- (void)resetPasswordForAccountWithEmail:(NSString *_Nullable)email
+{
+    NSString *invalidEmailMessage = [CMHInputValidators localizedValidationErrorMessageForEmail:email];
+    if (nil != invalidEmailMessage) {
+        [CMHAlerter displayAlertWithTitle:NSLocalizedString(@"", nil) andMessage:invalidEmailMessage inViewController:self];
+        return;
+    }
 }
 
 @end
