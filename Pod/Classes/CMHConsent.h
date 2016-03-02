fbbc90205fbce4747089d251b219226c4349f91f
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-03-02 16:41:39 -0500

    Allow developer to fetch the particpant's signature from a given consent

--- Pod/Classes/CMHConsent.h
+++ Pod/Classes/CMHConsent.h
@@ -1,9 +1,13 @@
 #import <CloudMine/CloudMine.h>
 #import <ResearchKit/ResearchKit.h>
 
+typedef void(^CMHFetchSignatureCompletion)(UIImage *_Nullable signature, NSError *_Nullable error);
+
 @interface CMHConsent : CMObject
 
 @property (nonatomic, nullable, readonly) ORKTaskResult *consentResult;
 @property (nonatomic, nullable, readonly) NSString *studyDescriptor;
+
+- (void)fetchSignatureImageWithCompletion:(_Nullable CMHFetchSignatureCompletion)block;
 
 @end
--- Pod/Classes/CMHConsent.m
+++ Pod/Classes/CMHConsent.m
@@ -4,6 +4,8 @@
 
 @implementation CMHConsent
 
+#pragma mark Internal
+
 - (_Nonnull instancetype)initWithConsentResult:(ORKTaskResult *)consentResult
                      andSignatureImageFilename:(NSString *)filename
                         forStudyWithDescriptor:(NSString *)descriptor
@@ -18,13 +20,48 @@
     return self;
 }
 
+#pragma mark Public
+
+- (void)fetchSignatureImageWithCompletion:(CMHFetchSignatureCompletion)block
+{
+    /* Return the memoized image in memory rather than re-fetching
+     * This is safe because we assume the signature image will never change
+     * after initially being uploaded. To be even more efficient, we could
+     * cache the image on disk. */
+    if (nil != self.signatureImage) {
+        if (nil != block) {
+            block(self.signatureImage, nil);
+        }
+
+        return;
+    }
+
+    [CMStore.defaultStore userFileWithName:self.signatureImageFilename additionalOptions:nil callback:^(CMFileFetchResponse *response) {
+        if (nil == block) {
+            return;
+        }
+
+        if (nil != response.error) {
+            block(nil, response.error); //TODO: Local error
+        }
+
+        if (nil == response.file.fileData) {
+            block(nil, nil); //TODO: create error
+        }
+
+        UIImage *image = [UIImage imageWithData:response.file.fileData];
+        self.signatureImage = image;
+        block(image, nil);
+    }];
+}
+
+#pragma mark NSCoding
+
 - (instancetype)initWithCoder:(NSCoder *)aDecoder
 {
     self = [super initWithCoder:aDecoder];
     if (nil == self) return nil;
 
-    // TODO: Pull the wrapper class, decode the object and ensure it is of
-    // the same type as the wrapper class. Set _consentResult to the wrapper.wrappedResult
     self.consentResult = [aDecoder decodeObjectForKey:@"consentResult"];
     self.signatureImageFilename = [aDecoder decodeObjectForKey:@"signatureImageFilename"];
     self.studyDescriptor = [aDecoder decodeObjectForKey:CMHStudyDescriptorKey];
