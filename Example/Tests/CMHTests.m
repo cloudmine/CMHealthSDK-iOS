ec2ea04db59b726cd27a48221763f6cfd330cacf
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-03-07 12:32:16 -0500

    Set up a header file for storing an API key and App Id for an integration test environment

--- Example/Tests/CMHTests.m
+++ Example/Tests/CMHTests.m
@@ -1,10 +1,22 @@
 #import <ResearchKit/ResearchKit.h>
+#import "CMHTest-Secrets.h"
 
 SpecBegin(CMHealth)
 
-describe(@"CMHealth", ^{
+describe(@"CMHealthIntegration", ^{
+
+    it(@"should have set up your App Id and API Key constants", ^{
+        NSLog(@"%@", CMHTestsAppId);
+
+        expect(CMHTestsAppId.length).to.beGreaterThan(0);
+        expect(CMHTestsAppId).notTo.equal(@"REPLACE_WITH_AN_APP_ID_TO_USE_FOR_TESTING");
+
+        expect(CMHTestsAPIKey.length).to.beGreaterThan(0);
+        expect(CMHTestsAPIKey).notTo.equal(@"REPLACE_WITH_API_KEY");
+    });
+
     it(@"should pass", ^{
-        expect(1 == 1).to.equal(YES);
+        expect(YES).to.beTruthy();
     });
 });
 
