487f2f43cd78d2f8e66175b246e64c60d1f6cbf4
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-03-10 12:19:08 -0500

    Add unit tests for success cases for consent validation

--- Example/Tests/CMHConsentValidatorTests.m
+++ Example/Tests/CMHConsentValidatorTests.m
@@ -107,6 +107,38 @@ describe(@"CMHConsentValidator", ^{
         expect(error).notTo.beNil();
         expect(error.code).to.equal(CMHErrorUserDidNotSign);
     });
+
+    it(@"should find a valid signature if it is at the top level of the results", ^{
+        NSError *error = nil;
+
+        ORKTaskResult *taskResult = [[ORKTaskResult alloc] initWithTaskIdentifier:@"CMHTestIdentifier" taskRunUUID:[NSUUID new] outputDirectory:nil];
+        ORKConsentSignatureResult *signatureResult = CMHConsentValidatorTestFactory.validSignatureResult;
+        taskResult.results = @[signatureResult];
+
+        ORKConsentSignature *signature = [CMHConsentValidator signatureFromConsentResults:taskResult error:&error];
+
+        expect(error).to.beNil();
+        expect(signature).to.equal(signatureResult.signature);
+    });
+
+    it(@"should find a valid signature if it is arbitrarily nested", ^{
+        NSError *error = nil;
+
+
+        ORKTaskResult *topResult = [[ORKTaskResult alloc] initWithTaskIdentifier:@"CMHTestIdentifier" taskRunUUID:[NSUUID new] outputDirectory:nil];
+        ORKTaskResult *firstResult = [[ORKTaskResult alloc] initWithTaskIdentifier:@"CMHTestIdentifier1" taskRunUUID:[NSUUID new] outputDirectory:nil];
+        ORKTaskResult *secondResult = [[ORKTaskResult alloc] initWithTaskIdentifier:@"CMHTestIdentifier2" taskRunUUID:[NSUUID new] outputDirectory:nil];
+        ORKStepResult *finalResult = [[ORKStepResult alloc] initWithStepIdentifier:@"" results:nil];
+
+        finalResult.results = @[ORKTextQuestionResult.new, CMHConsentValidatorTestFactory.validSignatureResult, ORKTextQuestionResult.new];
+        secondResult.results = @[finalResult];
+        topResult.results = @[firstResult, secondResult];
+
+        ORKConsentSignature *signature = [CMHConsentValidator signatureFromConsentResults:topResult error:&error];
+
+        expect(error).to.beNil();
+        expect(signature).to.equal([(ORKConsentSignatureResult *)[finalResult.results objectAtIndex:1] signature]);
+    });
 });
 
 SpecEnd
