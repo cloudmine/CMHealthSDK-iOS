b90421ad6f23a168bf6d7904be03a36ce99d1693
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-03-14 12:21:58 -0400

    Header appledoc comments for the root class of the SDK; closes #7

--- Pod/Classes/CMHealth.h
+++ Pod/Classes/CMHealth.h
@@ -8,8 +8,23 @@
 #import "ORKConsentSection+CMHealth.h"
 #import "CMHAuthViewController.h"
 
+/**
+ *  CMHealth is the easiest way to add secure, HIPAA compliant cloud data storage and user
+ *  management to your ResearchKit clinical study iOS app. Built and backed by CloudMine 
+ *  and the CloudMine Connected Health Cloud.
+ *
+ *  @see https://github.com/cloudmine/CMHealthSDK-iOS
+ */
 @interface CMHealth : NSObject
 
+/**
+ *  Configures global state needed to establish communication with the CloudMine backend.
+ *  Typically, this would be invoked in the `-application:didFinishLaunchingWithOptions:`
+ *  method of your app delegate class.
+ *
+ *  @param identifier The identifier for this app on the CloudMine backend
+ *  @param secret Your app secret, also referred to as an API Key
+ */
 + (void)setAppIdentifier:(NSString *_Nonnull)identifier appSecret:(NSString *_Nonnull)secret;
 
 @end
