001af213199c25b457df50bebb664570285a1ca8
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-06-10 14:04:44 -0400

    Add placeholder nullability annotations to unavailable declarations to prevent warning while building activity list

--- Pod/Classes/CMHActivityList.h
+++ Pod/Classes/CMHActivityList.h
@@ -3,8 +3,8 @@
 
 @interface CMHActivityList : CMObject
 
-- (instancetype)init NS_UNAVAILABLE;
-- (instancetype)initWithObjectId:(NSString *)theObjectId NS_UNAVAILABLE;
+- (_Null_unspecified instancetype)init NS_UNAVAILABLE;
+- (_Null_unspecified instancetype)initWithObjectId:(NSString *_Null_unspecified)theObjectId NS_UNAVAILABLE;
 
 @property (nonatomic, nonnull, readonly) NSArray <OCKCarePlanActivity *> * activities;
 - (_Nonnull instancetype)initWithActivities:(NSArray<OCKCarePlanActivity *> *_Nonnull)activities;
