daf5a703214abd5d4fc2209ed1b38a3f6507de3f
Ben DiFrancesco <ben@scopelift.co>
2017-03-20 11:44:06 -0400

    Set up barebones test harness for CareKit integration tests

--- Example/Tests/CMHCareIntegrationTests.m
+++ Example/Tests/CMHCareIntegrationTests.m
@@ -1,9 +1,41 @@
-//
-//  CMHCareIntegrationTests.m
-//  CMHealth
-//
-//  Created by Ben DiFrancesco on 3/20/17.
-//  Copyright © 2017 CloudMine, Inc. All rights reserved.
-//
+#import <CMHealth/CMHealth.h>
+#import "CMHTest-Secrets.h"
 
-#import <Foundation/Foundation.h>
+SpecBegin(CMHCareIntegration)
+
+describe(@"CMHCareIntegration", ^{
+    
+    beforeAll(^{
+        NSString *assertionError = @"You haven't set valid credentials in CMHTest-Secrets.h";
+        NSAssert(CMHTestsAppId.length > 0, assertionError);
+        NSAssert(![CMHTestsAppId isEqualToString:@"REPLACE_WITH_AN_APP_ID_TO_USE_FOR_TESTING"], assertionError);
+        NSAssert(CMHTestsAPIKey.length > 0, assertionError);
+        NSAssert(![CMHTestsAPIKey isEqualToString:@"REPLACE_WITH_API_KEY"], assertionError);
+        NSAssert(CMHTestsSharedSnippetName.length > 0, assertionError);
+        NSAssert(![CMHTestsSharedSnippetName isEqualToString:@"REPLACE_WITH_A_SNIPPET_FOR_SHARING_SAVED_CAREKIT_DATA_TO_USE_FOR_TESTING"], assertionError);
+        NSAssert(CMHTestsAsyncTimeout >= 1.0, @"An async timeout of less than 1 second is not advised; tests will run as fast as your connection allows regardless of the timeout value; lowering the value will not speed up test time and can lead to false failures.");
+        
+        [CMHealth setAppIdentifier:CMHTestsAppId appSecret:CMHTestsAPIKey sharedUpdateSnippetName:CMHTestsSharedSnippetName];
+        setAsyncSpecTimeout(CMHTestsAsyncTimeout);
+
+        waitUntil(^(DoneCallback done) {
+            if([CMHUser currentUser].isLoggedIn) {
+                [[CMHUser currentUser] logoutWithCompletion:^(NSError *error){
+                    done();
+                }];
+            } else {
+                done();
+            }
+        });
+        
+        NSAssert(![CMHUser currentUser].isLoggedIn, @"Failed to log user out before beginning test");
+    });
+    
+    it(@"Should run a true test", ^{
+        expect(YES).to.beTruthy();
+    });
+    
+});
+
+
+SpecEnd
