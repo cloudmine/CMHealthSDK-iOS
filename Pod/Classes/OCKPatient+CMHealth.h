2d49d934c9331429034891c859ac1fe0523df5fc
Ben DiFrancesco <ben@scopelift.co>
2017-07-21 10:30:07 -0400

    Header documentation for OCKPatient category data accessors

--- Pod/Classes/OCKPatient+CMHealth.h
+++ Pod/Classes/OCKPatient+CMHealth.h
@@ -6,7 +6,21 @@ extern NSString * const _Nonnull CMHPatientUserInfoPhotoIdKey;
 
 @interface OCKPatient (CMHealth)
 
+/**
+ *  This property will be populated for patient objects fetched by 
+ *  a Care Provider from an instance of `CMHCarePlanStore`.
+ *  It exposes the same `CMHUserData` that is created and configurable
+ *  by the patient through their `CMHUser` instance.
+ */
 @property (nonatomic, nullable, readonly) CMHUserData *cmh_patientUserData;
+
+/**
+ *  Fetch the patient's profile image, if one exists, or nil if not.
+ *  This method is meant for use in conjunction with patient objects fetched by
+ *  an instance of `CMHCarePlanStore` in a Care Provider app context.
+ *
+ *  @param block Executes when image has been fetched successfully or fails with an error.
+ */
 - (void)cmh_fetchProfileImageWithCompletion:(_Nullable CMHFetchProfileImageCompletion)block;
 
 @end
